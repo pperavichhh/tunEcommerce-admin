e43f9744254e417c46333387ab64924f6402169c
pperavichhh <pperavichhh@gmail.com>
2024-02-11 23:08:01 +0700

    final

---
+++ providers/modal-providers.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { useEffect , useState } from "react"
+import { StoreModal } from "@/components/modals/store-modal"
+import { AlertModal } from "@/components/modals/alert-modal";
+
+export const ModalProvider = () => {
+    const [isMounted , setIsMounted] = useState(false);
+
+    useEffect(() =>{
+        setIsMounted(true)
+    }, []);
+    if (!isMounted){
+        return null;
+    }
+    return(
+        <>
+            <StoreModal />
+        </>
+    );
+} ;
